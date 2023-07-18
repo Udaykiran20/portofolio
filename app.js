6a4f7ea738a7d7ea4e88f67ed9152a35181ea15d
uday kiran <114855288+Udaykiran20@users.noreply.github.com>
2023-07-18 11:14:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+
+function java(){
+    let info =document.getElementById('info')
+    if(info.style.display==='none'){
+        info.style.display='block';
+    }
+   else{
+    info.style.display='none';
+   }
+    
+    
+}
+function sql(){
+    let para=document.getElementById('123')
+    if(para.style.display==="none"){
+        para.style.display='block';
+    }
+    else{
+        para.style.display='none'
+    }
+}
+script=()=>{
+    let script=document.getElementById('1234')
+    if(script.style.display==="none"){
+        script.style.display='block';
+    }
+    else{
+        script.style.display='none'
+    }
+}
+html=()=>{
+    let concepts_html=document.getElementById('concepts_html')
+    if(concepts_html.style.display==='none'){
+        concepts_html.style.display='block';
+    }
+    else{
+        concepts_html.style.display='none'
+    }
+}
+section_css=()=>{
+    let concepts_css=document.getElementById('concepts_css')
+    if(concepts_css.style.display==='none'){
+        concepts_css.style.display='block';
+    }
+    else{
+        concepts_css.style.display='none'
+    }
+}
